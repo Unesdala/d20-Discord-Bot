f6f30ad4d57c82f5cf4da40edc3e953f36302d11
Rhayven <codingforllamas@gmail.com>
2018-04-21 23:31:15 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -10,10 +10,12 @@ logger.level = 'debug';
 // Initialize Discord Bot
 
 // Random Number Generator Function
-// Allows the bot to generate the numbes for the d20 roll
+// Allows for the bot to actually generate the rolls
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
 var bot = new Discord.Client({
     token: auth.token,
     autorun: true
@@ -24,7 +26,7 @@ bot.on('ready', function(evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function(user, userID, channelID, message, evt) {
-    // Bot needs to listen for commands.
+    // Lets the bot listen for commands.
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
@@ -39,7 +41,14 @@ bot.on('message', function(user, userID, channelID, message, evt) {
                     message: getRandomInt(21)
                 });
                 break;
-                // Add case commands below here
+                // !d100
+            case 'd100':
+                bot.sendMessage({
+                    to: channelID,
+                    message: getRandomInt(101)
+                });
+                break;
+                // Add case commands after here.
         }
     }
 });
